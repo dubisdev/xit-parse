b21beb70454194405c34a172056fc108ba8a452b
dubisdev <dubisdev@gmail.com>
2025-07-26 12:59:46 +0200

    chore: add base Task item

---
+++ src/TaskItem.ts
@@ -0,0 +1,51 @@
+import { TaskItemStatus } from "./TaskItemStatus";
+
+type TaskItemPrimitives = {
+    content: string;
+    status: string;
+    priority: number;
+    tags: string[];
+    dueDate: Date | null;
+};
+
+export class TaskItem {
+    private readonly _content: string;
+    private readonly _status: TaskItemStatus;
+    private readonly _priority: number;
+    private readonly _tags: string[];
+    private readonly _dueDate: Date | null;
+
+    private constructor({
+        content,
+        status,
+        priority,
+        tags,
+        dueDate
+    }: TaskItemPrimitives) {
+        this._content = content;
+        this._status = new TaskItemStatus(status);
+        this._priority = priority;
+        this._tags = tags;
+        this._dueDate = dueDate;
+    }
+
+
+    /**
+     * Count of ! of the task item.
+     * As the spec says, more ! means higher priority.
+     */
+    get priority(): number {
+        return this._priority;
+    }
+
+    /**
+     * List item content.
+     * - Can be multiline.
+     * - Can be empty or full of spaces.
+     */
+    get content(): string {
+        return this._content;
+    }
+
+
+}
